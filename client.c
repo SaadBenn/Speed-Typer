dd52b78c27c27ca1cc7391c29fe8512013ba6154
Saad <saadbennmushtaq@gmail.com>
2018-06-15 01:08:12 -0500

    Complete functinality of teh folowing:
    - pass word from server to client
    - pass word from client to server
    - Do the checking for winner
    - Send the appropriate winner/loser message to teh clients
    - end the game.

--- client.c
+++ client.c
@@ -8,83 +8,176 @@
 #include <pthread.h>
 #include<stdbool.h>
 
+// forward reference
+int spawn_threads();
+//pthread_t threads[2];
+
+
+char word_buffer[10];
+bool ready_for_word = true;
+pthread_cond_t read_word_signal;
+bool done = false;
+int status_read;
+
+int fd;
+char word[5];
+bool signaled = false;
+pthread_cond_t signalAvailable;
+pthread_mutex_t signalLock;
+char message_length;
+char pid_client[10];
+//char curr_pid[10];
 const char SERVER_FIFO[] = "./_pipe";
 
 int main() {
-    char message[10];
-
+    
     // Create Client FIFO
     printf("Creating client's FIFO!\n");
 
-    sprintf(message, "%d", getpid());
-    char *fifo_name = (char *) malloc (sizeof(SERVER_FIFO) + sizeof(message) + 1);
+    sprintf(pid_client, "%d", getpid());
+    char *fifo_name = (char *) malloc (sizeof(SERVER_FIFO) + sizeof(pid_client) + 1);
     strcat(fifo_name, SERVER_FIFO);
-    strcat(fifo_name, message);
+    strcat(fifo_name, pid_client);
 
     printf("Client creating a named pipe %s\n", fifo_name);
 
     int send = mkfifo(fifo_name, 0666);
     if (send < 0) {
-        printf("Client FIFO failed to create. It likely already exists and you can ignore this warning!");
+        printf("Client FIFO failed to create. It likely already exists and you can ignore this warning!\n");
     
     } else {
         printf("Created Client FIFO!\n");
     }
 
-    printf("Client %s\n", message);
+    printf("Client pid is: %s\n", pid_client);
     
     //Open server fifo for registering the client
-    int fd = open(SERVER_FIFO, O_WRONLY);
+    fd = open(SERVER_FIFO, O_WRONLY);
     if (fd < 0) {
-        perror("Cannot open fifo");
+        perror("Cannot open server's fifo\n");
         return EXIT_FAILURE;
     }
 
-    char message_length = (char)strlen(message);
+    message_length = (char)strlen(pid_client);
+    
+    printf("The client's pid is: %s\n", pid_client);
     // Send string's length to the server
     write(fd, &message_length, 1);
     // Send string characters               
-    write(fd, message, 10); 
-    //close(fd);
+    write(fd, &pid_client, message_length); 
+
+    printf("%s\n", "Mama there goes your man.");
+
+    spawn_threads(fifo_name);
+
+    unlink(fifo_name);
+    return EXIT_SUCCESS;
+}
 
 
-    // Get whether accepted into game or not
-    char string_buffer[100];
-    char word[5];
+void* incoming_messages(void *arg) {
+    char *fifo_name = (char*) arg;
 
-    int read_server_message = open(fifo_name, O_RDONLY);
-    if (read_server_message < 0) {
-        printf("Client FIFO could not be opened properly!");
-        return EXIT_FAILURE;
+    int read_fifo_status = open(fifo_name, O_RDONLY);
+    if (read_fifo_status < 0) {
+        perror("Error! Cannot open clients's pipe");
+        pthread_exit(0);
     }
 
-    bool player_ready = false;
-    char server_res_length;
-    
-    while(!player_ready) {
-        // get the message_length o fthe message
-        int reads_ = read(read_server_message, &server_res_length, 1);
-        if (reads_ == 0) {
-            break;
+    char length;
+    while (!done) {
+        int status_ = read(read_fifo_status, &length, 1);
+
+        if (status_ == 0) {
+            printf("%s\n", "Eror reading the client\n");
+            pthread_exit(0);
         }
 
-        // read the actual message
-        read(read_server_message, string_buffer, server_res_length);
-        //string_buffer[(int)message_length] = '\0';
-        printf("%s\n", string_buffer);
+        read(read_fifo_status, &word_buffer, length);
+        if (strcmp(word_buffer, "WINNER") == 0) {
+            done = true;
+            printf("%s\n", "WINNER!");
         
-        player_ready = true;
+        } else if (strcmp(word_buffer, "LOSER") == 0) {
+            done = true;
+            printf("%s\n", "LOSER");
+        
+        }
+        pthread_mutex_lock(&signalLock);
+        signaled = true;
+        pthread_cond_signal(&signalAvailable);
+        pthread_mutex_unlock(&signalLock);
     }
 
-    printf("Congratulations! We are now ready to play!!\n");
+    pthread_exit(0);
+}
 
-    read(read_server_message, word, 5);
-    printf("%s\n", word);
 
-    // spawn the threads
+void* incoming_stdin(void *arg) {
+
+    char *spelled_word;
     
+    while (!done) {
+
+        pthread_mutex_lock(&signalLock);
+        while (!signaled) {
+            pthread_cond_wait(&signalAvailable, &signalLock);
+        }
+
+        signaled = false;
+        pthread_mutex_unlock(&signalLock);
+
+        if (done) {
+            pthread_exit(0);
+        }
+
+        spelled_word = malloc(sizeof(10));
+        printf("Spell the word: %s\n", word_buffer);
+        fgets(spelled_word, 10, stdin);
+        spelled_word[strlen(spelled_word) -1] = '\0';
+        //printf("%s\n", );
+        
+        printf("We got the user input in thread 2 %s\n", spelled_word);
+
+        int server_status = open(SERVER_FIFO, O_WRONLY);
+        if (server_status < 0) {
+            perror("Error opening server fifo.");
+            pthread_exit(0);
+        }
+        
+        char *word_pid = malloc(strlen(spelled_word) + message_length + 2);
+        
+        // write spelled word + " " + pid to the server's pipe 
+        strcat(word_pid, spelled_word);
+        strcat(word_pid, " ");
+        strcat(word_pid, pid_client);
+        
+        printf("The word + pid in the client is: %s\n", word_pid);
+
+        char word_pid_length = strlen(word_pid);
+        write(server_status, &word_pid_length, 1);
+        int success = write(server_status, word_pid, word_pid_length);
+        if (success < 0) {
+            perror("ERROR: writing to the server failed in the client");
+        }
+    } 
+    pthread_exit(0);
+}
+
+
+
+int spawn_threads(char *fifo_name) {
+
+    pthread_t threads[2];
+
+    pthread_cond_init(&signalAvailable, NULL);
+    pthread_mutex_init(&signalLock, NULL);
     
-    // //Tidy
-    close(read_server_message);
-    return EXIT_SUCCESS;
+    pthread_create(&threads[0], NULL, &incoming_messages, (void *) fifo_name);
+    pthread_create(&threads[1], NULL, &incoming_stdin, NULL);
+    
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+
+    return 0;
 }
--- server.c
+++ server.c
@@ -9,21 +9,38 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdbool.h> // bool
 
 #define NUMPLAYERS 2
 #include "words.h"
 
-// forward reference
+// forward references
 int signal_client(int player);
 int generate_random_word();
+int open_client_pipe(int player);
+int read_client_response();
+void game_starter();
+
 // store the name of the pipes
 char *client_pipes[NUMPLAYERS];
 
+int counter = 0;
 const char SERVER_FIFO[] = "./_pipe";
 char string_buffer[10];
 
+char *random_word;
+char word_from_client[10];
+char word_from_client2[10];
+int server_file_des;
+bool winner_decided = false;
 char *client_pids[NUMPLAYERS];
+char client_word[15];
+
+
+int pid_index;
 int client_scores[NUMPLAYERS] = {0, 0};
+char *track_client_word[2];
+char *fifo_name;
 
 int main()
 {
@@ -32,11 +49,12 @@ int main()
     int status = mkfifo(SERVER_FIFO, 0666);
     if (status < 0) {
         printf("FIFO creation failed! It is likely that this FIFO already exists! \n");
+        return EXIT_FAILURE;
     }
 
     // Open FIFO for write
-    int response = open(SERVER_FIFO, O_RDONLY);
-    if (response < 0) {
+    server_file_des = open(SERVER_FIFO, O_RDONLY);
+    if (server_file_des < 0) {
         printf("Response FIFO is unavailable for read access");
         return EXIT_FAILURE;
     
@@ -48,115 +66,129 @@ int main()
     char length;
 
     int count = 0;
-    while (count < NUMPLAYERS)
-    {
-    	// read the length
-        int reads = read(response, &length, 1);
-        read(response, string_buffer, length);
+    while (count < NUMPLAYERS) {
 
+    	printf("%s\n", "Waiting for clients pids");
+    	// read the length
+        int stat_len = read(server_file_des, &length, 1);
+
+        if (stat_len < 0) {
+         	perror("Error reading the length\n");
+     	 	return EXIT_FAILURE;
+        }
+
+        int stat_buf = read(server_file_des, &string_buffer, length);
+        if (stat_buf < 0) {
+        	perror("Error reading the string buffer\n");
+        	return EXIT_FAILURE;
+        }
+
+        // null terminate the byte
         string_buffer[(int)length] = '\0';
         
         if (string_buffer[0] != '\0') {
             client_pids[count] = (char *)malloc(sizeof(string_buffer));
-            client_pids[count] = string_buffer;
+            client_pids[count] = strdup(string_buffer);
+			printf("Stored the client pid at %d %s\n", count, client_pids[count]);
             printf("Registered Client: %s\n", string_buffer);
-            
-            int errnum = signal_client(count);
-            if (errnum < 0) {
-                 printf("Failed to notify client properly!");
-                 return EXIT_FAILURE;
-            }
-            count++;
         }
+        count++;
     }
-    printf("Clients Registered Succesfully!\n");
 
-    // Write to client FIFOs
-    // int wordsSpelledCorrectly = 0;
+    printf("Clients Registered Succesfully!\n\n\n");
 
-    // int i;
-    // for (i = 0; i < NUMPLAYERS; i++) {
-    // 	//strcat(clientFifos[i], SERVER_FIFO);
-    // 	//strcat(clientFifos[i], client_pids[i]);
-    // 	int _success = open(client_pipes[i], O_WRONLY);
-    // 	if (_success < 0) {
-    // 		perror("Failed to open the client's pipe!!");
-    // 	}
-    // 	printf("Server opening client's pipe again %s\n", client_pipes[i]);    
-    // }
 
-    // Check if word was spelled correctly using the client_responses FIFO
+    //printf("Client's pipe name is: %s\n", client_pids[0]);
+    //printf("Client's pipe name is: %s\n", client_pids[1]);
 
-    // Send all clients win or lose through the FIFOS created by clients
-    
+    game_starter();
+    printf("%s\n", "Game over!");
     return 0;
 }
 
-//************************************************************
-// int signal_client(int player)
-// {
-//     // Signal registration
-//     //char *SERVER_FIFO = "/tmp/_pipe";
-//     char *fifo_name = (char *)malloc(sizeof(SERVER_FIFO) + sizeof(client_pclient_client_pids[player]) + 1);
-//     strcat(fifo_name, SERVER_FIFO);
-//     strcat(fifo_name, client_pclient_client_pids[player]);
-    
-//     int signal_result = open(fifo_name, O_WRONLY);
-    
-//     if (signal_result < 0) {
-//         printf("Could not find client FIFO!");
-//         return EXIT_FAILURE;
-    
-//     } else {
-//         char *message = "You have been accepted into the game! \n";
-//         char L = (char)strlen(message);
-//         write(signal_result, &L, 1);                          //Send string length
-//         write(signal_result, message, (char)strlen(message)); //Send string characters
-//         close(signal_result);
-//     }
-//     return EXIT_SUCCESS;
-// }
+void game_starter() {
+	int i;
+	char length;
+	int rand_num;
+	for (i = 0; i < NUMPLAYERS; i++) {
+		int client_fifo_status = open_client_pipe(i);
+		rand_num = generate_random_word();
+		//printf("Random number is: %d\n",  rand_num);
+		// save the word for comparision with the client's response
+	 	track_client_word[i] = strdup(words[rand_num]);
+	 	//printf("The random word is: %s\n", track_client_word[i]);
+	 	random_word = strdup(words[rand_num]);
+	 	length = strlen(words[rand_num]);
+	 	write(client_fifo_status, &length, 1);
+	 	write(client_fifo_status, words[rand_num], length);
+	}
 
+	char client_word_length;
+	while (!winner_decided) {
+		printf("%s\n", "Waiting for client to type the word!");
+		read(server_file_des, &client_word_length, 1);
+		read(server_file_des, word_from_client, client_word_length);
+		word_from_client[client_word_length] = '\0';
+		printf("Got the word: %s from the client in the server\n", word_from_client);
+		
+		char *user_word = strtok(word_from_client, " ");
+		char *user_pid = strtok(NULL, " ");
 
+		for (i = 0; i < NUMPLAYERS; i++) {
 
-int signal_client(int player)
-{
-    // Signal registration
-    //char *fileNameFirst = "./_pipe";
-    char *fifo_name = (char *)malloc(sizeof(SERVER_FIFO) + sizeof(client_pids[player]) + 1);
-    strcat(fifo_name, SERVER_FIFO);
-    strcat(fifo_name, client_pids[player]);
+			if (strcmp(client_pids[i], user_pid) == 0) {
+				pid_index = i;
+				
+				if (strcmp(track_client_word[i], user_word) == 0) {
+					(client_scores[i])++;
+				}
 
-    //printf("Clients pids %s\n", client_pids[0]);
-    
-    printf("Server opening client's pipe %s\n", fifo_name);
+				if (client_scores[i] == 10) {
+					winner_decided = true;
+				} 
+			}		
+		}
 
-    int signalEntry = open(fifo_name, O_WRONLY);
-    if (signalEntry < 0) {
-        printf("Could not find client FIFO!");
-        return EXIT_FAILURE;
-    
-    } else {
-    	
-    	// we got the correct name of the pipe
-    	client_pipes[player] = (char *)malloc(sizeof(SERVER_FIFO) + sizeof(client_pids[player]) + 1);
-    	client_pipes[player] = fifo_name;
-    	//printf("I stored this pid %s\n", client_pipes[player]);
+		if (!winner_decided) {
+			rand_num = generate_random_word();
+			
+			int _status_ = open_client_pipe(pid_index);
+			if (_status_ < 0) {
+				perror("Error: error in opening the client's pipe.\n");
+			}
 
-        char *message = "You have been accepted into the game! \n";
-        char L = (char)strlen(message);
-        write(signalEntry, &L, 1);                          //Send string length
-        write(signalEntry, message, (char)strlen(message)); //Send string characters
+			track_client_word[pid_index] = strdup(words[rand_num]);
+			char len_new = strlen(words[rand_num]);
+			write(_status_, &len_new, 1);
+			write(_status_, words[rand_num], len_new);
+		
+		} else {
+			char len_result;
+			for (i = 0; i < NUMPLAYERS; i++) {
+				if (client_scores[i] == 10) {
+					len_result = strlen("WINNER");
+					
+					int signalEntry = open_client_pipe(i);
+					if (signalEntry < 0) {
+						perror("Error opening the client's pipe.\n");
+					}
 
-        // write the word to the pipe as well
-        int rand_num = generate_random_word();
-        //printf("%d\n", rand_num);
-        //printf("%s\n", words[rand_num]);
+					write(signalEntry, &len_result, 1);
+					write(signalEntry, "WINNER", len_result);
+				} else {
+					len_result = strlen("LOSER");
+					int signalEntry = open_client_pipe(i);
+					if (signalEntry < 0) {
+						perror("Error opening the client's pipe.\n");
+					}
 
-        write(signalEntry, words[rand_num], 4);
-        close(signalEntry);
-    }
-    return EXIT_SUCCESS;
+					write(signalEntry, &len_result, 1);
+					write(signalEntry, "LOSER", len_result);
+				}	
+			}
+		}
+		
+	}
 }
 
 
@@ -166,4 +198,19 @@ int generate_random_word() {
 	int rand_num = rand() % len;
 
 	return rand_num;
+}
+
+
+int open_client_pipe(int player) {
+
+	fifo_name = (char *)malloc(sizeof(SERVER_FIFO) + sizeof(client_pids[player]) + 1);
+    strcat(fifo_name, SERVER_FIFO);
+    strcat(fifo_name, client_pids[player]);
+
+    //printf("Clients pids %s\n", client_pids[0]);
+    
+    printf("Server opening client's pipe %s\n", fifo_name);
+
+    int signalEntry = open(fifo_name, O_WRONLY);
+    return signalEntry;
 }
